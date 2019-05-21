b37112b1efc8c8c4d4a61de62043fc3102e8ea7e
gregvers <gregvers@gmail.com>
2019-05-20 17:13:34 -0700

    update

--- chatroom.py
+++ chatroom.py
@@ -6,7 +6,7 @@ from pubnub.enums import PNOperationType, PNStatusCategory, PNReconnectionPolicy
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub, SubscribeListener
 from pubnub.callbacks import SubscribeCallback
-import datetime
+import datetime, json
 
 pubkey = 'pub-c-c408ce7d-fe0d-4f9a-aae9-e67d5c9b6bd9'
 subkey = 'sub-c-efaf056a-75c7-11e9-b68a-fa5eae84dea0'
@@ -28,19 +28,33 @@ class MySubscribeCallback(SubscribeCallback):
     def presence(self, pubnub, presence):
         pass
 
-def init_chatroom():
+def init_chatroom(nickname):
     pnconfig = PNConfiguration()
     pnconfig.reconnect_policy = PNReconnectionPolicy.LINEAR
     pnconfig.publish_key = pubkey
     pnconfig.subscribe_key = subkey
+    pnconfig.uuid = nickname
     pubnub = PubNub(pnconfig)
     pubnub.add_listener(MySubscribeCallback())
-    pubnub.subscribe().channels('mychatroom').execute()
+    pubnub.subscribe().channels('mychatroom').with_presence().execute()
     return pubnub
 
 def post_msg(pubnub, nickname, msg):
     pubnub.publish().channel('mychatroom').message({'nickname': nickname, 'msg': msg, 'time': str(datetime.datetime.now())}).sync()
 
+def here_now_callback(result, status):
+    if status.is_error():
+        return
+    for channel_data in result.channels:
+        print("number of participants: %s" % channel_data.occupancy)
+    for occupant in channel_data.occupants:
+        print("uuid: %s" % (occupant.uuid))
+
+def list_participants(pubnub):
+    pubnub.here_now() \
+        .channels('mychatroom') \
+        .include_uuids(True) \
+        .pn_async(here_now_callback)
 
 ######## MAIN #########
 def main(argv):
@@ -51,11 +65,13 @@ def main(argv):
         usage()
         sys.exit()
 
-    pubnub = init_chatroom()
+    pubnub = init_chatroom(args.nickname)
     while True:
         msg = input()
-        post_msg(pubnub, args.nickname, msg)
-
+        if msg == "list participants":
+            list_participants(pubnub)
+        else:
+            post_msg(pubnub, args.nickname, msg)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
